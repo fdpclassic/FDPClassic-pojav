56ac82f1935dae3f1964bd4be92b280796953195
whats this? <105464811+fdpmc@users.noreply.github.com>
2022-07-01 04:35:29 +0100

    free crash

--- src/main/java/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -22,7 +22,7 @@ class ModuleManager : Listenable {
 
     val modules = mutableListOf<Module>()
     private val moduleClassMap = hashMapOf<Class<*>, Module>()
-
+    fun getModuleInCategory(category: ModuleCategory) = modules.filter { it.category == category }
     var pendingBindModule: Module? = null
 
     init {
@@ -126,7 +126,7 @@ class ModuleManager : Listenable {
     @EventTarget
     private fun onKey(event: KeyEvent) {
         if (pendingBindModule == null) {
-            modules.filter { it.triggerType == EnumTriggerType.TOGGLE && it.keyBind == event.key }.forEach { it.toggle() }
+            modules.toMutableList().filter { it.triggerType == EnumTriggerType.TOGGLE && it.keyBind == event.key }.forEach { it.toggle() }
         } else {
             pendingBindModule!!.keyBind = event.key
             ClientUtils.displayAlert("Bound module §a§l${pendingBindModule!!.name}§3 to key §a§l${Keyboard.getKeyName(event.key)}§3.")
