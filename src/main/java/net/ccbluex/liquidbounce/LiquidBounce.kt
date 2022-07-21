27e781262f9298cb4f4c40b62ebb9a4da35f0335
whats this? <105464811+gatooooooo@users.noreply.github.com>
2022-07-21 04:36:57 +0100

    yeet

--- src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -45,7 +45,7 @@ object LiquidBounce {
     const val MINECRAFT_VERSION = "1.8.9"
     var Darkmode = false
     const val VERSIONTYPE = "classic"
-    const val FDPVERSION = "2.0b"
+    const val FDPVERSION = "2.1a"
 
     @JvmField
     val gitInfo = Properties().also {
